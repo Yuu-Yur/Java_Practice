91ed131ac285bf8080439ecd2f00d78db710a41d
Yuu-Yur <gkcjdqls@gmail.com>
2024-10-18 16:16:06 +0900

    first commit

---
+++ Java_Practice/src/javaPractice241017/FirstDay.java
@@ -0,0 +1,10 @@
+package javaPractice241017;
+
+public class FirstDay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("여러가지 설정과 깃헙 연습");
+	}
+
+}
