a4f8151b78e346e3e8047651a1c4e1d7d28ccbae
swdean07 <swdean07@gmail.com>
2024-10-18 16:29:54 +0900

    홍상우 그룹실습 테스트 실시.

---
+++ Java_Practice/src/javaPractice241018/Hsw861009.java
@@ -0,0 +1,10 @@
+package javaPractice241018;
+
+public class Hsw861009 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("단체 실습 테스트.");
+	}
+
+}
