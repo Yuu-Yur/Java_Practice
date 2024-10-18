c8a6091da042c248c8523f96ad5e221b913e665d
Yuu-Yur <gkcjdqls@gmail.com>
2024-10-18 16:17:27 +0900

    깃 푸시 테스트

---
+++ Java_Practice/src/javaPractice241018/GitPushTest.java
@@ -0,0 +1,10 @@
+package javaPractice241018;
+
+public class GitPushTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("깃 푸시 테스트");
+	}
+
+}
